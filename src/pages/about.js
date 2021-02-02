8a582dbc9fa61a0993812ca22513b48dd5e3a863
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-02 16:31:04 -0500

    Add styled components

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../components/layout';
+import headshot from '../images/headshot.png';
+// import logo from '../images/gatsby-icon.png'
+
+const About = () => {
+    return ( 
+        <Layout>
+            <div>
+                <h1>Hi! I'm Karen</h1>
+                <img src={headshot} alt="Karen in business attire"/>
+                <p>From my earliest memories, I’ve always loved to learn. I taught myself to play guitar and built a car from scratch with my father as a teenager. I’ve studied multiple languages and I picked up HTML and CSS in college. For years I built sites for family and friends until deciding to pivot my career to software development. </p>
+                <p>A recent graduate of the Software Engineering Immersive at General Assembly, I am experienced in modern web design. Skilled in pure CSS and several frameworks, I build alluring sites and apps with great functionality.</p>
+                <p>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. Passionate about languages, travel, and fitness, I look to make a positive difference in the world with software development.</p>
+            </div>
+        </Layout>
+    );
+}
+ 
+export default About;
