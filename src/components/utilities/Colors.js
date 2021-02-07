d0e475b82ba16ec2ba7ff0203d019631c32e8a20
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-07 09:50:35 -0600

    Add type system and color variables

---
+++ src/components/utilities/Colors.js
@@ -0,0 +1,6 @@
+export const teal = '#007f97';
+export const pine = '#a4bcbc';
+export const mauve = '#7c587f';
+export const violet = '#4c3f77';
+export const black = '#051126';
+export const white = '#f9feff';
