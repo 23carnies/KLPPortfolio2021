58e2485d891e82733bd22c7aa64fe9b2fe8cfbd6
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-06 22:30:00 -0600

    Add flexcenter function

---
+++ src/components/utilities/index.js
@@ -0,0 +1,8 @@
+export const FlexCenter = ({dir = 'row', wrap = 'no wrap'}) => {
+    return css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: ${dir};
+    flex-wrap: ${wrap};
+`};
