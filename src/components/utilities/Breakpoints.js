b8fea462147daf1768c4279972bbd9482efb31b7
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-09 09:43:16 -0500

    Add breakpoint function

---
+++ src/components/utilities/Breakpoints.js
@@ -0,0 +1,20 @@
+import { css } from 'styled-components';
+
+const size = {
+    xXLarge = 1920,
+    xLarge = 1440,
+    large = 1280,
+    medium = 1024,
+    small = 768,
+    xSmall = 640
+}
+
+export const below = Object.keys(size).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+        @media (max-width: ${size[label]}px) {
+            ${css(...args)}
+        }
+    `;
+    return acc;
+}, {})
+console.log('below', below);
