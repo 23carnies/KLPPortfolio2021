64953738d52beba3ef66ef3a6a04da3299de148a
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-09 09:52:56 -0500

    Correct size array in breakpoints

--- src/components/utilities/Breakpoints.js
+++ src/components/utilities/Breakpoints.js
@@ -1,12 +1,12 @@
 import { css } from 'styled-components';
 
 const size = {
-    xXLarge = 1920,
-    xLarge = 1440,
-    large = 1280,
-    medium = 1024,
-    small = 768,
-    xSmall = 640
+    xXLarge: 1920,
+    xLarge: 1440,
+    large: 1280,
+    medium: 1024,
+    small: 768,
+    xSmall: 640
 }
 
 export const below = Object.keys(size).reduce((acc, label) => {
