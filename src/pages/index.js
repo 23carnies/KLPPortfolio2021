d9c741c289f9a8be1d59f6c1b2a1ea62a7501046
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-12 16:49:15 -0500

    Remove NamePlate to its own component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from 'styled-components';
+import { Link, useStaticQuery, graphql } from "gatsby"
+import NamePlate from '../components/nameplate';
+import { FlexCenter, teal, violet, white } from '../components/utilities';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <NamePlate 
+      siteTitle={data.site.siteMetadata.title}
+    />
+    <Main>
+      <About><Link to="/about">Check Me Out!</Link></About>
+      <Projects><Link to="/projects">Stuff I've Built</Link></Projects>
+      <Contact><Link to="/contact">I'm Available for Hire!</Link></Contact>
+    </Main>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
+
+const Main = styled.article`
+  ${FlexCenter};
+`;
+
+const Section = styled.section`
+  height: 100vh;
+  width: 33.33vw;
+  ${FlexCenter};
+`;
+
+const About = styled(Section)`
+  background: ${violet};
+  
+`;
+const Projects = styled(Section)`
+  background: ${teal};
+
+`;
+const Contact = styled(Section)`
+  background: ${white};
+
+`;
