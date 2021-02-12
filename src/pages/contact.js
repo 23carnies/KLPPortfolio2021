1c284032a64d7ca01c584a21e47a1610626eab78
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-12 16:24:19 -0500

    Add basic contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header';
+import Layout from '../components/layout';
+
+const Contact = () => {
+    return ( 
+        <Layout>
+            <Header />
+
+        </Layout>
+     );
+}
+ 
+export default Contact;
