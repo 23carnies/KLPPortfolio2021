6fd6203454b16f6dc187a588065753c557cc5bd5
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-12 17:50:23 -0500

    Change text size

--- src/components/utilities/Type.js
+++ src/components/utilities/Type.js
@@ -1,4 +1,4 @@
 export const nameplate = '5rem "Warnes", display';
 export const navItem = '2rem "Quicksand", sans-serif';
-export const text = '2.4rem "Quicksand", sans-serif';
+export const text = '2rem "Quicksand", sans-serif';
 export const title = '4rem "Rock Salt", handwriting';
