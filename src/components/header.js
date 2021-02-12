a660e0e9acd13918d331192c8b56b37b554334ec
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-12 16:22:36 -0500

    Remove nameplate effect

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,12 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import * as S from "./styledComponents"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   
   <S.Header>
-      <S.Effect>
-        <S.Name to="https://www.linkedin.com/in/karen-lobin-perkins/">{siteTitle}</S.Name>
-        <S.Gradient></S.Gradient>
-        <S.Dodge></S.Dodge>
-      </S.Effect>
     <S.Nav>
-      {/* <S.Effect type="about">
-        <S.About to="/about">About</S.About>
-        <S.Gradient></S.Gradient>
-        <S.Dodge></S.Dodge>
-      </S.Effect>
-      <S.Effect>
-        <S.Projects to="/projects">Projects</S.Projects>
-        <S.Gradient></S.Gradient>
-        <S.Dodge></S.Dodge>
-      </S.Effect>
-      <S.Effect>
-        <S.Contact to="/contact">Contact</S.Contact>
-        <S.Gradient></S.Gradient>
-        <S.Dodge></S.Dodge>
-      </S.Effect> */}
       <S.NavItem type="about" to="/about">About</S.NavItem>
       <S.NavItem to="/projects">Projects</S.NavItem>
       <S.NavItem to="/contact">Contact</S.NavItem>
