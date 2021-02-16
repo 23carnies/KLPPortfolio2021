ee2c798bee3a2c49487b80b67cc29a9e0fe8b6d4
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-15 19:27:25 -0500

    Rearrange project order
    Move all styled components into file

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
+import Layout from '../components/layout';
 import Header from '../components/header';
 import ProjectCard from '../components/projectCard';
-import Layout from '../components/layout';
-import { CardGroup } from '../components/styledComponents'
+
+import styled from 'styled-components';
+import { Flex, pine, violet, title } from '../components/utilities';
 
 class Projects extends Component {
     state = { 
         projects: [
-            {
-                title: 'Meet Your Maker',
-                description: 'A full CRUD MERN stack app connecting urban farmers and makers (like myself) with local buyers. A team project at General Assembly utilizing AWS-SDK and Semantic UI for React.',
-                image: 'https://i.imgur.com/t7IPbkV.png',
-                openLink: 'https://meetyourmaker925.herokuapp.com/',
-                gitLink: 'https://github.com/23carnies/MeetYourMakerCoupled',
-            },
             {
                 title: 'Despejado',
                 description: 'A bilingual weather app built in React, utilizing two APIs and designed in Sass. A passion project built in December 2020.',
                 image: 'https://i.imgur.com/G8S7QY5.jpg',
                 openLink: 'http://despejado.surge.sh/',
                 gitLink: 'https://github.com/23carnies/despejado',
+            },
+            {
+                title: 'Meet Your Maker',
+                description: 'A full CRUD MERN stack app connecting urban farmers and makers (like myself) with local buyers. A team project at General Assembly utilizing AWS-SDK and Semantic UI for React.',
+                image: 'https://i.imgur.com/t7IPbkV.png',
+                openLink: 'https://meetyourmaker925.herokuapp.com/',
+                gitLink: 'https://github.com/23carnies/MeetYourMakerCoupled',
             },
             {
                 title: 'This Portfolio',
@@ -55,22 +57,43 @@ class Projects extends Component {
         return ( 
             <Layout>
                 <Header />
-                <CardGroup>
-                {this.state.projects.map((project, idx) =>
-                    <ProjectCard 
-                        key={idx}
-                        title={project.title}
-                        description={project.description}
-                        image={project.image}
-                        openLink={project.openLink}
-                        gitLink={project.gitLink}
-                    />
-                )}
+                <ProjectBack>
+                    <Headline>My work</Headline>
+                    <CardGroup>
+                    {this.state.projects.map((project, idx) =>
+                        <ProjectCard 
+                            key={idx}
+                            title={project.title}
+                            description={project.description}
+                            image={project.image}
+                            openLink={project.openLink}
+                            gitLink={project.gitLink}
+                        />
+                    )}
 
-                </CardGroup>
+                    </CardGroup>
+
+                </ProjectBack>
             </Layout>
          );
     }
 }
  
 export default Projects;
+
+const ProjectBack = styled.article`
+    ${Flex({fd:'column'})}
+    background: ${pine};
+`;
+
+const Headline = styled.h4`
+    font: ${title};
+    color: ${violet};
+    border-bottom: 5px dotted ${violet};
+`;
+
+const CardGroup = styled.section`
+    ${Flex({fw:'wrap'})};
+    max-width: 1400px;
+    height: 100vh;
+`;
