b434282c83b656b2d81de7e78b76ba1522c55099
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-15 19:26:14 -0500

    Move styled components into file

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,20 +1,75 @@
 import React from 'react'
-import * as S from './styledComponents';
+import styled from 'styled-components';
+
+import { Flex, teal, white, violet, cardBorder, box_shadow3 } from '../components/utilities';
 
 const ProjectCard = ({ title, description, image, openLink, gitLink }) => {
     return ( 
-        <S.Layout>
-            <S.Card>
-                <S.Image src={image}></S.Image>
-                <S.HTwo>{title}</S.HTwo>
-                {/* <S.Pgraph>{description}</S.Pgraph> */}
+        // <Layout>
+            <Card>
+                <Image src={image}></Image>
+                <HTwo>{title}</HTwo>
+                {/* <Pgraph>{description}</Pgraph> */}
                 <div>
-                    <S.BTN type="open" src={openLink}>Open</S.BTN>
-                    <S.BTN src={gitLink}>Github</S.BTN>
+                    <BTN type="open" src={openLink}>Open</BTN>
+                    <BTN src={gitLink}>Github</BTN>
                 </div>
-            </S.Card>
-        </S.Layout>
+            </Card>
+        // </Layout>
      );
 }
  
 export default ProjectCard;
+
+const Card = styled.div`
+    ${Flex({fd:'column'})};
+    margin: 20px;
+    padding: 20px;
+    width: 370px;
+    height: 500px;
+    border: ${cardBorder};
+    border-radius: 70px 3px 70px 3px;
+    background: ${teal};
+    box-shadow: ${box_shadow3};
+`;
+
+const Image = styled.img`
+    max-width: 225px;
+    border: ${cardBorder};
+    border-radius: 5px;
+`;
+
+
+// const Pgraph = styled.p`
+//     font: 400 18px 'Quicksand', sans-serif;
+//     text-align: center;
+//     line-height: 24px;
+//     margin: 5px;
+//     color: ${white};
+// `;
+
+const HTwo = styled.h2`
+    font: 700 36px 'Roboto Mono', monospace;
+    text-align: center;
+    line-height: 54px;
+    margin: 5px;
+    color: ${white};
+`;
+
+const BTN = styled.button`
+    width: 100px;
+    height: 50px;
+    margin: 10px;
+
+    border: ${cardBorder};
+    border-radius: 5px;
+    font: 600 18px 'Quicksand', sans-serif;
+
+    color: ${({type}) => (type === 'open' ? `${white}` : `${violet}`)};
+    background: ${({type}) => (type === 'open' ? `${violet}` : `${white}`)};
+
+    &hover {
+        color: ${({type}) => (type !== 'open' ? `${white}` : `${violet}`)};
+        background: ${({type}) => (type !== 'open' ? `${violet}` : `${white}`)};
+    }
+`;
