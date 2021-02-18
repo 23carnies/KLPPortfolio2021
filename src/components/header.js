043524e73300603ec040b89ddace38a95c77ea03
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-18 15:23:54 -0500

    Add logo to nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'gatsby';
 import { Flex, navItem } from './utilities';
+import kpLogo from '../images/logo.svg'
+
 
 const Header = () => (
   <Head>
     <Nav>
-      <NavItem to="/">Home</NavItem>
+      <Logo src={kpLogo} alt="KP Phoenix Logo"/>
+      <NavItem home to="/">Home</NavItem>
       <NavItem type="about" to="/about">About</NavItem>
       <NavItem to="/projects">Projects</NavItem>
       <NavItem to="/contact">Contact</NavItem>
@@ -30,6 +33,12 @@ const Nav = styled.nav`
     height: 80px;
 `;
 
+const Logo = styled.img`
+    border-radius: 50%;
+    width: 70px;
+    margin-top: -7px;
+`;
+
 const NavItem = styled(Link)`
     text-decoration: none;
     font: ${navItem};
@@ -37,4 +46,8 @@ const NavItem = styled(Link)`
     text-transform: uppercase;
     padding: 0 20px;
     margin-left: ${({type}) => (type === 'about' ? 'auto' : '0')};
+    
+    ${props => props.home && css`
+      padding-left: 0;
+    `}
 `;
