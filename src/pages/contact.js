f2e65cb39514e4112b56a267eff86f4e92a5f15a
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-20 07:59:41 -0500

    Update color scheme

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,27 +3,34 @@ import styled from 'styled-components';
 import Header from '../components/header';
 import Layout from '../components/layout';
 import ContactMe from '../components/emailjs';
-import { title, pine } from '../components/utilities';
+import { Flex, title, yellow3, orange3 } from '../components/utilities';
 
 
 const Contact = () => {
     return ( 
         <Layout>
             <Header />
-            <div style={{margin:'0 auto', textAlign: 'center'}}>
+            <Box>
                 <Headline>Let's work together!</Headline>
                 <ContactMe />
-            </div>
+            </Box>
         </Layout>
      );
 }
  
 export default Contact;
 
+const Box = styled.div`
+    ${Flex({fd:'column'})};
+    width: 100%;
+    text-align: center;
+    background: ${orange3};
+`;
+
 const Headline = styled.h4`
     text-align: center;
     font: ${title};
-    color: ${pine};
-    border-bottom: 5px dotted ${pine};
+    color: ${yellow3};
+    border-bottom: 5px dotted ${yellow3};
     margin: 0;
 `;
