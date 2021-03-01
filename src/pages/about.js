fb082c9a526a7a5f8019f9ad1f2e303ae08b6087
23carnies <46233990+23carnies@users.noreply.github.com>
2021-03-01 13:53:37 -0500

    Change color and image

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Layout from '../components/layout';
 import Header from '../components/header';
-// import headshot from '../images/headshot.png';
-import detroit from '../images/klp-detroit.png';
-import { Flex, orange3, yellow3, imgBorder, title, text } from '../components/utilities'
-// import styled from 'styled-components'
+import { Flex, orange1,orange3, yellow3, imgBorder, title, text } from '../components/utilities'
 
 const About = () => {
     return ( 
@@ -14,12 +11,11 @@ const About = () => {
             <AboutBack>
             <Headline>Hi! I'm Karen</Headline>
             <Box>
-                <Img src={detroit} alt="Karen "/>
-                {/* <Headshot src={headshot} alt="Karen in business attire"/> */}
+                <Img src="https://i.imgur.com/XxhHjhz.jpg" alt="Karen "/>
                 <div style={{minWidth: '50%'}}>
-                    <Text>From my earliest memories, I’ve always loved to learn. I taught myself to play guitar and built a car from scratch with my father as a teenager. I’ve studied multiple languages and I picked up HTML and CSS in college. For years I built sites for family and friends until deciding to pivot my career to software development. </Text>
-                    <Text>A recent graduate of the Software Engineering Immersive at General Assembly, I am experienced in modern web design. Skilled in pure CSS and several frameworks, I build alluring sites and apps with great functionality.</Text>
-                    <Text>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. Passionate about languages, travel, and fitness, I look to make a positive difference in the world with software development. I want to see the world, and see what positive change I can be in it.</Text>
+                    <AboutText>From my earliest memories, I’ve always loved to learn. I taught myself to play guitar and built a car from scratch with my father as a teenager. I’ve studied multiple languages and I picked up HTML and CSS in college. For years I built sites for family and friends until deciding to pivot my career to software development. </AboutText>
+                    <AboutText>A recent graduate of the Software Engineering Immersive at General Assembly, I am experienced in modern web design. Skilled in pure CSS and several frameworks, I build alluring sites and apps with great functionality.</AboutText>
+                    <AboutText>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. Passionate about languages, travel, and fitness, I look to make a positive difference in the world with software development. I want to see the world, and see what positive change I can be in it.</AboutText>
 
                     <Quote>“Here is the world. Beautiful and terrible things will happen. Don't be afraid.”<br/>
                     <i>-- Frederick Buechner</i></Quote>
@@ -34,13 +30,15 @@ export default About;
 
 
 const AboutBack = styled.div`
-    width: 100vw;
+    max-width: 1440px;
+    margin: 0 auto;
     ${Flex({fd:'column'})};
-    background: ${orange3};
+    /* background: ${orange1}; */
+    /* background: ${orange3}; */
 `;
 
 const Box = styled.div`
-    ${Flex}
+    ${Flex({ai:'flex-start'})}
 `;
 
 const Headline = styled.h4`
@@ -52,17 +50,19 @@ const Headline = styled.h4`
 `;
 
 const Img = styled.img`
-    margin: 20px;
-    max-width: 45%;
+    margin: 40px 20px;
+    width: 30%;
+    border-radius: 12px;
+    /* max-width: 45%; */
 `;
 
-const Text = styled.p`
+const AboutText = styled.p`
     font: ${text};
     max-width: 95%;
-    margin-left: 2%;
+    margin: 40px 20px;
 `;
 
-const Quote = styled(Text)`
+const Quote = styled(AboutText)`
     font-weight: 700;
 `;
 
