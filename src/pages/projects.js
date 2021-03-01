a532842b9aff23777ac8d2bf56036b1ed3c5ede9
23carnies <46233990+23carnies@users.noreply.github.com>
2021-03-01 13:58:40 -0500

    Change background color

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,7 +13,7 @@ class Projects extends Component {
                 title: 't.ux',
                 description: 't.ux is the original, seminal product from theCOOP. I spearheaded the front end design implementation working closely with the design team to create pixel-perfect creations of their wireframes. I constructed a styled component library for the project and am in the process of implementing WAI-ARIA accessibility standards.',
                 image: 'https://tux-staging.herokuapp.com/images/vertical-logo.svg',
-                openLink: 'https://tux-staging.herokuapp.com/',
+                openLink: 'https://tux-staging.herokuapp.com/login',
                 gitLink: 'https://github.com/23carnies/tux',
                 alt: 'Logo for project pronounced Tux'
             },
@@ -96,7 +96,7 @@ export default Projects;
 
 const ProjectBack = styled.article`
     ${Flex({fd:'column'})}
-    background: ${black};
+    /* background: ${black}; */
     padding: 0 0 8%;
 `;
 
