aabded1d469007503afc8073951af1601c3ebc78
23carnies <46233990+23carnies@users.noreply.github.com>
2021-03-08 07:58:05 -0500

    Add font sizes

--- src/components/utilities/Type.js
+++ src/components/utilities/Type.js
@@ -1,7 +1,8 @@
 export const nameplate = '4rem "Warnes", display';
 export const navItem = '2rem "Quicksand", sans-serif';
 export const text = '1.7rem "Quicksand", sans-serif';
-export const boldType = '800 1rem "Quicksand", sans-serif';
+export const boldType = '800 1.3rem "Quicksand", sans-serif';
+export const descript = '800 1rem "Quicksand", sans-serif';
 export const label = '800 1.5rem "Quicksand", sans-serif';
 export const smLabel = '800 1rem "Quicksand", sans-serif';
 export const title = '"Rock Salt", handwriting';
