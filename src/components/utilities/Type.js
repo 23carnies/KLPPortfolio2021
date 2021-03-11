39c5777462a8c29de251fe20b865f95eed1ca173
23carnies <46233990+23carnies@users.noreply.github.com>
2021-03-11 09:43:04 -0500

    Change nameplate fontsize

--- src/components/utilities/Type.js
+++ src/components/utilities/Type.js
@@ -1,5 +1,5 @@
 // export const nameplate = '4rem "Warnes", display';
-export const nameplate = '2rem "Warnes", display';
+export const nameplate = '2.5rem "Warnes", display';
 export const navItem = '500 2rem "Quicksand", sans-serif';
 export const text = '1.7rem "Quicksand", sans-serif';
 export const boldType = '800 1.3rem "Quicksand", sans-serif';
