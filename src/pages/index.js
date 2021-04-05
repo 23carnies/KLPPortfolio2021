6a45df7b1f40a48b8fff2580341fdaf27857c187
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-05 16:29:21 -0400

    Redesign entirely
    remove 3 panel layout, replace with stacked components

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,17 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import Bird from '../images/phoenix.svg';
+import Contact from "../components/contatct";
+import About from "../components/about";
 
 const IndexPage = () => (
   
   <Layout>
     <SEO title="Home" />
-    {/* <NamePlate /> */}
-    <Main>
+    <About />
+    <Contact />
+    {/* <Main>
       
-      {/* About Me Section */}
       <About>
         <Link to="/about">
           <h4>Find out more<br/> about me here</h4>
@@ -25,7 +27,6 @@ const IndexPage = () => (
           <Text type="quote">"Our greatest glory is not in never failing, but in rising every time we fall." <br/><i>--Confucious</i></Text>
       </About>
 
-      {/* Projects Section */}
       <Projects>
         <Link to="/projects">
           <h3>I built this...</h3>
@@ -35,12 +36,10 @@ const IndexPage = () => (
           <Text descript>t.ux is the original product from theCOOP. I spearheaded the front end design implementation constructing a styled component library for the project and am in the process of implementing WAI-ARIA accessibility standards.</Text>
           <Link to="https://github.com/23carnies/tux"><Text github>t.ux Github repo</Text></Link>
         </Tux>
-          {/* <SmallCard /> */}
           <Link to="/projects"><Text projects type="more">Check out more projects here.</Text></Link>
 
         </Projects>
 
-      {/* Contact Section */}
       <Contact>
           <ContactDiv>
             <a rel="noreferrer" target="_blank" href="https://github.com/23carnies" alt="Github logo, opens new window to Karen's Github page"><LgGithub /></a>
@@ -54,115 +53,115 @@ const IndexPage = () => (
           <Text type="more">I am looking for a Front End Engineering<br/> role at a team-focused company.</Text>
       </Contact>
 
-    </Main>
+    </Main> */}
   </Layout>
 )
 
 export default IndexPage;
 
-const Main = styled.section`
-  ${Flex};
-  ${below.medium`
-    ${Flex({fd:'column'})}
-  `}
-`;
+// const Main = styled.section`
+//   ${Flex};
+//   ${below.medium`
+//     ${Flex({fd:'column'})}
+//   `}
+// `;
 
-const Section = styled.section`
-  height: 75vh;
-  width: 33.33vw;
-  ${Flex};
-  ${below.xLarge`
-    height: 75vh;
-    width: 100vw;
-  `};
-`;
+// const Section = styled.section`
+//   height: 75vh;
+//   width: 33.33vw;
+//   ${Flex};
+//   ${below.xLarge`
+//     height: 75vh;
+//     width: 100vw;
+//   `};
+// `;
 
-const About = styled(Section)`
-  display: grid;
-  grid-template-rows: repeat(3,1fr);
-  background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
+// const About = styled(Section)`
+//   display: grid;
+//   grid-template-rows: repeat(3,1fr);
+//   background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
   
-`;
+// `;
 
-const Phoenix = styled.img`
-  width: 280px;
-  grid-row: 2/span 1;
-  z-index: 10;
-  transform: rotate(5deg) scaleX(-1);
-  margin: 0 auto;
-  ${below.small`
-    width: 225px;
-  `}
-`;
+// const Phoenix = styled.img`
+//   width: 280px;
+//   grid-row: 2/span 1;
+//   z-index: 10;
+//   transform: rotate(5deg) scaleX(-1);
+//   margin: 0 auto;
+//   ${below.small`
+//     width: 225px;
+//   `}
+// `;
 
-const Text = styled.p`
-  font: ${boldType};
-  color: ${black};
-  margin: 0;
-  padding: 10px;
-  grid-row: 3/span 1;
-  background: ${({ type }) => ((type === 'quote') || (type === 'more') ? 'rgba(255,255,255,.2)' : 'none')};
-  border-radius: 8px;
-  ${below.large`
-    background: ${({ type }) => (type === 'quote' ? 'rgba(255,255,255,.2)' : 'none')};
+// const Text = styled.p`
+//   font: ${boldType};
+//   color: ${black};
+//   margin: 0;
+//   padding: 10px;
+//   grid-row: 3/span 1;
+//   background: ${({ type }) => ((type === 'quote') || (type === 'more') ? 'rgba(255,255,255,.2)' : 'none')};
+//   border-radius: 8px;
+//   ${below.large`
+//     background: ${({ type }) => (type === 'quote' ? 'rgba(255,255,255,.2)' : 'none')};
     
-  `}
+//   `}
 
-  ${props => props.github && css`
-    text-decoration: underline;
-    margin-top: 5px;
-  `}
-  ${props => props.projects && css`
-    text-decoration: underline;
-    margin-top: 6%;
-  `}
-  ${props => props.descript && css`
-    font: ${descript};
-  `}
-`;
+//   ${props => props.github && css`
+//     text-decoration: underline;
+//     margin-top: 5px;
+//   `}
+//   ${props => props.projects && css`
+//     text-decoration: underline;
+//     margin-top: 6%;
+//   `}
+//   ${props => props.descript && css`
+//     font: ${descript};
+//   `}
+// `;
 
-const Projects = styled(Section)`
-  ${Flex({fd:'column',jc:'flex-start'})};
-  background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
-  position: relative;
-  ${below.small`
-    ${Flex({fd:'column',jc:'center'})};
-  `}
-  `;
+// const Projects = styled(Section)`
+//   ${Flex({fd:'column',jc:'flex-start'})};
+//   background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
+//   position: relative;
+//   ${below.small`
+//     ${Flex({fd:'column',jc:'center'})};
+//   `}
+//   `;
 
-const Tux = styled.div`
-  width: 75%;
-  border: 1px solid ${yellow3};
-  height: 50%;
-  background-color: rgba(255,255,255,.25);
-  border-radius: 15px;
-  z-index: 15;
-  ${below.large`
-    height: 60%;
-  `};
-  ${below.small`
-    width: 95%;
-    height: 60%;
-    padding: 2%;
-  `};
-`;
+// const Tux = styled.div`
+//   width: 75%;
+//   border: 1px solid ${yellow3};
+//   height: 50%;
+//   background-color: rgba(255,255,255,.25);
+//   border-radius: 15px;
+//   z-index: 15;
+//   ${below.large`
+//     height: 60%;
+//   `};
+//   ${below.small`
+//     width: 95%;
+//     height: 60%;
+//     padding: 2%;
+//   `};
+// `;
 
-const TuxImg = styled.img`
-  width: 100%;
-  margin: 0 auto;
-  padding: 5%;
-`;
+// const TuxImg = styled.img`
+//   width: 100%;
+//   margin: 0 auto;
+//   padding: 5%;
+// `;
 
 
-const ContactDiv = styled.div`
-  ${Flex};
-  max-width: 30vw;
-  ${below.large`
-  `}
-`;
+// const ContactDiv = styled.div`
+//   ${Flex};
+//   max-width: 30vw;
+//   ${below.large`
+//   `}
+// `;
 
-const Contact = styled(Section)`
-  display: ${Flex({fd:'column'})};
-  background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
-  padding: 25px;
-`;
+// const Contact = styled(Section)`
+//   display: ${Flex({fd:'column'})};
+//   background: linear-gradient(to top, #ff99cc 8%,  ${violet} 100%);
+//   padding: 25px;
+// `;
